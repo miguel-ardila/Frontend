722ccf6f6d44d06c878ed0a0db02fb857b6d8fc7
miguel-ardila <miguel_sxe@hotmail.com>
2022-09-08 20:26:03 -0500

    color circle

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ function createCircle(radian) {
 
     ctx.beginPath();
     var gradient = ctx.createLinearGradient(0, 0, 230, 230);
-    gradient.addColorStop(0, "#ff0000");
-    gradient.addColorStop(0.5, "#ff7300");
-    gradient.addColorStop(1, "#fff300");
+    gradient.addColorStop(0, "#009092");
+    gradient.addColorStop(0.5, "#00c6c7");
+    gradient.addColorStop(1, "#8bffff");
     ctx.arc(115, 115, 90, 0, radian, false);
     ctx.strokeStyle = gradient;
     ctx.lineWidth = 30;
